bb53933128245f5a9feac685589fd0b0b6e738b4
Алексей Жигимонт <C:\Users\foto-\AppData\Roaming\The Bat!>
2018-04-17 16:55:08 +0300

    Fix bags

--- task1/src/main/java/com/example/task1/HotelUI.java
+++ task1/src/main/java/com/example/task1/HotelUI.java
@@ -1,22 +1,19 @@
 package com.example.task1;
 
 import java.util.List;
-
 import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.server.Sizeable.Unit;
 import com.vaadin.shared.ui.ValueChangeMode;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.renderers.HtmlRenderer;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser
@@ -27,77 +24,114 @@ import com.vaadin.ui.VerticalLayout;
  * intended to be overridden to add component to the user interface and
  * initialize non-component functionality.
  */
+@SuppressWarnings("serial")
 @Theme("mytheme")
 public class HotelUI extends UI {
 
+	final HorizontalLayout filterForm = new HorizontalLayout();
+	final HorizontalLayout contentAll = new HorizontalLayout();
+	final HorizontalLayout content = new HorizontalLayout();
+	final HorizontalLayout buttons = new HorizontalLayout();
 	final VerticalLayout layout = new VerticalLayout();
 	final HotelService hotelService = HotelService.getInstance();
 	final Grid<Hotel> hotelGrid = new Grid<Hotel>(Hotel.class);
-	final TextField filter = new TextField("Search hotels by name:");
-	final TextField filter1 = new TextField("Search hotels by address:");
+	final TextField filterByName = new TextField("Search hotel by name:");
+	final TextField filterByAddress = new TextField("Search hotel by address:");
 	final Button addHotel = new Button("Add hotel");
 	final Button deleteHotel = new Button("Delete hotel");
-	private HotelEditForm form = new HotelEditForm(this);
+	final Button editHotel = new Button("Edit hotel");
+	private HotelEditForm editForm = new HotelEditForm(this);
 
 	@Override
 	protected void init(VaadinRequest vaadinRequest) {
 		setContent(layout);
-		setWidth(100, Unit.PERCENTAGE);
-		HorizontalLayout controls = new HorizontalLayout();
-		HorizontalLayout controls2 = new HorizontalLayout();
-		HorizontalLayout formss = new HorizontalLayout();
-		controls.addComponents(filter, filter1);
-		form.setVisible(false);
-		deleteHotel.setEnabled(false);
-		HorizontalLayout content = new HorizontalLayout();
-		formss.addComponents(content, form);
+		// Add all layouts
+		layout.addComponents(filterForm, hotelGrid, editForm, buttons);
 
-		form.setWidth(400, Unit.PIXELS);
-		layout.addComponents(controls, formss, controls2);
-		controls2.addComponents(addHotel, deleteHotel);
-		content.addComponents(hotelGrid);
-		content.setWidth(100, Unit.PERCENTAGE);
-		layout.setWidth(100, Unit.PERCENTAGE);
+		filterForm.addComponents(filterByName, filterByAddress);
+		buttons.addComponents(addHotel, editHotel, deleteHotel);
 
-		
-		hotelGrid.setColumnOrder("name", "address", "rating", "category");
-		hotelGrid.removeColumn("url");
+		/*
+		 * Configuration for all contents
+		 */
+		contentAll.setWidth(100, Unit.PERCENTAGE);
 		hotelGrid.setWidth(100, Unit.PERCENTAGE);
+		editForm.setWidth(100, Unit.PERCENTAGE);
+		deleteHotel.setEnabled(false);
+		editHotel.setEnabled(false);
+		editForm.setVisible(false);
+
+		hotelGrid.setColumnOrder("name", "address", "rating", "category");
+		hotelGrid.addColumn(hotel -> "<a href='" + hotel.getUrl() + "' target='_top'>" + hotel.getName() + "</a>",
+				new HtmlRenderer()).setCaption("Web site");
+		hotelGrid.removeColumn("url");
+
+		/*
+		 * Buttons settings
+		 */
+		// SelectTable and EditButton configurations
 		hotelGrid.asSingleSelect().addValueChangeListener(e -> {
-			if (e.getValue() != null) {
+			if (e.getValue() != null && !editForm.isVisible()) {
 				deleteHotel.setEnabled(true);
-				form.setHotel(e.getValue());
+				editHotel.setEnabled(true);
+				// EditButton ClickListener
+				editHotel.addClickListener(event -> {
+					editForm.setHotel(e.getValue());
+					hotelGrid.setHeightByRows(4);
+					editForm.getDelete().setVisible(true);
+					deleteHotel.setVisible(false);
+					//filterForm.setVisible(false);
+					editHotel.setVisible(false);
+					addHotel.setVisible(false);
+				});
+			} else if (editForm.isVisible()) {
+				editForm.setHotel(e.getValue());
 			}
 		});
-
+		// DeleteButton configuration
 		deleteHotel.addClickListener(e -> {
 			Hotel delCandidate = hotelGrid.getSelectedItems().iterator().next();
 			hotelService.delete(delCandidate);
 			deleteHotel.setEnabled(false);
+			editHotel.setEnabled(false);
+			editForm.setVisible(false);
+			filterForm.setVisible(true);
+			editHotel.setVisible(true);
+			addHotel.setVisible(true);
 			updateList();
-			form.setVisible(false);
 		});
-		updateList();
-		filter.addValueChangeListener(e -> updateList());
-		filter1.addValueChangeListener(e -> updateList());
-		filter1.setValueChangeMode(ValueChangeMode.LAZY);
-		filter.setValueChangeMode(ValueChangeMode.LAZY);
-		addHotel.addClickListener(e -> form.setHotel(new Hotel()));
+		// AddButton configuration
+		addHotel.addClickListener(e -> {
+			editForm.getDelete().setVisible(false);
+			hotelGrid.setHeightByRows(4);
+			deleteHotel.setVisible(false);
+			//filterForm.setEnabled(false);
+			editHotel.setVisible(false);
+			addHotel.setVisible(false);
+			editForm.setHotel(new Hotel());
+		});
 
+		/*
+		 * Filters settings
+		 */
+		updateList();
+		filterByName.addValueChangeListener(e -> updateList());
+		filterByAddress.addValueChangeListener(e -> updateList());
+		filterByName.setValueChangeMode(ValueChangeMode.LAZY);
+		filterByAddress.setValueChangeMode(ValueChangeMode.LAZY);
 	}
 
 	public void updateList() {
 		List<Hotel> hotelList = hotelService.findAll();
-		if (filter.getValue() != "") {
-			hotelList = hotelService.findAll(filter.getValue(), "name");
+		if (filterByName.getValue() != "") {
+			hotelList = hotelService.findAll(filterByName.getValue(), "name");
 		} else {
-			hotelList = hotelService.findAll(filter1.getValue(), "address");
+			hotelList = hotelService.findAll(filterByAddress.getValue(), "address");
 		}
-		if (filter1.getValue() != "" && filter.getValue() != "") {
-			hotelList = hotelService.findAll(filter.getValue(), filter1.getValue(), "addressname");
+		if (filterByName.getValue() != "" && filterByAddress.getValue() != "") {
+			hotelList = hotelService.findAll(filterByName.getValue(), filterByAddress.getValue(), "addressname");
 		}
 		hotelGrid.setItems(hotelList);
-
 	}
 
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
